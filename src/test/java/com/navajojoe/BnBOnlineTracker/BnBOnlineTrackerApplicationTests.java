ffbcb84fd04f95a6899ad82c20cb9b1c585bf951
btruchan1023 <btruchan@wisc.edu>
2021-04-07 17:28:42 -0500

    Initial commit for BnBOnlineTracker. This was made using spring intializr on start.spring.io
    The spring dependencies used were Spring Web, Spring Data JPA, and H2

---
+++ src/test/java/com/navajojoe/BnBOnlineTracker/BnBOnlineTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navajojoe.BnBOnlineTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnBOnlineTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
